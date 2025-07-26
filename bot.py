40985187ae02dbb8d4619b8b76f04b4d3f153481
5hojib <yesiamshojib@gmail.com>
2025-07-26 13:10:46 +0000

    format: auto-format code by ruff.
    Signed-off-by: 5hojib <yesiamshojib@gmail.com>

--- bot.py
+++ bot.py
@@ -1,5 +1,4 @@
 from pyrogram import Client, filters
-import os
 
 API_ID = "23272804"
 API_HASH = "bbd029520a44a558573fb76375c6d681"
@@ -7,6 +6,7 @@ BOT_TOKEN = "8016890106:AAECqZzikJ6DkXXSkEsNAKU5r9qVWUpwbMA"
 DOMAIN = "https://LocalHost:800"
 
 app = Client("FileToLinkBot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+
 
 @app.on_message(filters.document | filters.video | filters.audio)
 async def save_file(client, message):
@@ -18,19 +18,23 @@ async def save_file(client, message):
     link = f"{DOMAIN}/file/{message.message_id}/{filename}"
     await message.reply_text(f"🔗 Your File Link:\n{link}")
 
+
 app.run()
 
-#gdrive_uploader 
-from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+# gdrive_uploader
 from gdrive_uploader import upload_to_gdrive
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-API_ID = 23272804    # Replace with your API_ID
-API_HASH = "bbd029520a44a558573fb76375c6d681" # Replace with your API_HASH
-BOT_TOKEN = "8016890106:AAECqZzikJ6DkXXSkEsNAKU5r9qVWUpwbMA" # Replace with your bot token
+API_ID = 23272804  # Replace with your API_ID
+API_HASH = "bbd029520a44a558573fb76375c6d681"  # Replace with your API_HASH
+BOT_TOKEN = (
+    "8016890106:AAECqZzikJ6DkXXSkEsNAKU5r9qVWUpwbMA"  # Replace with your bot token
+)
 
 bot = Client("leechbot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 temp_files = {}
+
 
 @bot.on_message(filters.document)
 async def receive_file(bot, message):
@@ -40,9 +44,16 @@ async def receive_file(bot, message):
     await message.reply(
         "Do you want GDrive Link?",
         reply_markup=InlineKeyboardMarkup(
-            [[InlineKeyboardButton("📤 Yes, Upload to GDrive", callback_data=f"gup_{message.id}")]]
-        )
+            [
+                [
+                    InlineKeyboardButton(
+                        "📤 Yes, Upload to GDrive", callback_data=f"gup_{message.id}"
+                    )
+                ]
+            ]
+        ),
     )
+
 
 @bot.on_callback_query(filters.regex("gup_"))
 async def handle_upload(bot, query):
@@ -60,5 +71,6 @@ async def handle_upload(bot, query):
         await query.message.edit(f"✅ File Uploaded:\n{link}")
     else:
         await query.message.edit("❌ Upload failed.")
+
 
 bot.run()
